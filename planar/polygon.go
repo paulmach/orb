140f5712c20e0b1f281ad9d86e15764ade772011
Paul Mach <paulmach@gmail.com>
2017-04-21 16:02:29 -0700

    Add planar.Polygon

---
+++ planar/polygon.go
@@ -0,0 +1,207 @@
+package planar
+
+import "math"
+
+// Polygon is a closed area. The first LineString is the outer ring.
+// The others are the holes. Each LineString is expected to be closed
+// ie. the first point matches the last.
+type Polygon []LineString
+
+// NewPolygon creates a new Polygon.
+func NewPolygon() Polygon {
+	return Polygon{}
+}
+
+// Centroid computes the area based centroid of the outer ring.
+func (p Polygon) Centroid() Point {
+	ring := p[0]
+	centroid := Point{}
+	area := 0.0
+	for i := 0; i < len(ring)-1; i++ {
+		a := ring[i][0]*ring[i+1][1] - ring[i+1][0]*ring[i][1]
+		area += a
+
+		centroid[0] += (ring[i][0] + ring[i+1][0]) * a
+		centroid[1] += (ring[i][1] + ring[i+1][1]) * a
+	}
+
+	// last vertext
+	i := len(ring) - 1
+	a := ring[i][0]*ring[0][1] - ring[0][0]*ring[i][1]
+	area += a
+	centroid[0] += (ring[i][0] + ring[0][0]) * a
+	centroid[1] += (ring[i][1] + ring[0][1]) * a
+
+	area /= 2
+	centroid[0] /= 6 * area
+	centroid[1] /= 6 * area
+
+	return centroid
+}
+
+// Contains checks if the point is within the polygon.
+// Points on the boundary are considered in.
+func (p Polygon) Contains(point Point) bool {
+	c := lineStringContains(p[0], point)
+	if !c {
+		return false
+	}
+
+	for i := 1; i < len(p); i++ {
+		if lineStringContains(p[i], point) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Area computes the positive area of the polygon minus the area
+// of the holes.
+func (p Polygon) Area() float64 {
+	area := lineStringArea(p[0])
+
+	for i := 1; i < len(p); i++ {
+		// minus holes
+		area -= lineStringArea(p[i])
+	}
+
+	return area
+}
+
+// Bound returns a bound around the polygon.
+func (p Polygon) Bound() Rect {
+	return p[0].Bound()
+}
+
+// Equal compares two polygons. Returns true if lengths are the same
+// and all points are Equal.
+func (p Polygon) Equal(polygon Polygon) bool {
+	if len(p) != len(polygon) {
+		return false
+	}
+
+	for i, ring := range p {
+		if !ring.Equal(polygon[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Clone returns a new deep copy of the polygon.
+// All of the rings are also cloned.
+func (p Polygon) Clone() Polygon {
+	np := make(Polygon, 0, len(p))
+	for _, ring := range p {
+		np = append(np, ring.Clone())
+	}
+
+	return np
+}
+
+func lineStringArea(ls LineString) float64 {
+	if len(ls) == 0 {
+		return 0
+	}
+
+	area := 0.0
+	for i := 1; i < len(ls)-1; i++ {
+		area += ls[i][0] * (ls[i+1][1] - ls[i-1][1])
+	}
+
+	// for i == N-1
+	last := len(ls) - 1
+	area += ls[last][0] * (ls[0][1] - ls[last-1][1])
+
+	// for i == N
+	area += ls[0][0] * (ls[1][1] - ls[last][1])
+
+	return math.Abs(area / 2.0)
+}
+
+func lineStringContains(ls LineString, point Point) bool {
+	if !ls.Bound().Contains(point) {
+		return false
+	}
+
+	c, on := rayIntersect(point, ls[0], ls[len(ls)-1])
+	if on {
+		return true
+	}
+
+	for i := 0; i < len(ls)-1; i++ {
+		inter, on := rayIntersect(point, ls[i], ls[i+1])
+		if on {
+			return true
+		}
+
+		if inter {
+			c = !c
+		}
+	}
+
+	return c
+}
+
+// Original implementation: http://rosettacode.org/wiki/Ray-casting_algorithm#Go
+func rayIntersect(p, s, e Point) (intersects, on bool) {
+	if s[0] > e[0] {
+		s, e = e, s
+	}
+
+	if p[0] == s[0] {
+		if p[1] == s[1] {
+			// p == start
+			return false, true
+		} else if s[0] == e[0] {
+			// vertical segment (s -> e)
+			// return true if within the line, check to see if start or end is greater.
+			if s[1] > e[1] && s[1] >= p[1] && p[1] >= e[1] {
+				return false, true
+			}
+
+			if e[1] > s[1] && e[1] >= p[1] && p[1] >= s[1] {
+				return false, true
+			}
+		}
+
+		// Move the y coordinate to deal with degenerate case
+		p[0] = math.Nextafter(p[0], math.Inf(1))
+	} else if p[0] == e[0] {
+		if p[1] == e[1] {
+			// matching the end point
+			return false, true
+		}
+
+		p[0] = math.Nextafter(p[0], math.Inf(1))
+	}
+
+	if p[0] < s[0] || p[0] > e[0] {
+		return false, false
+	}
+
+	if s[1] > e[1] {
+		if p[1] > s[1] {
+			return false, false
+		} else if p[1] < e[1] {
+			return true, false
+		}
+	} else {
+		if p[1] > e[1] {
+			return false, false
+		} else if p[1] < s[1] {
+			return true, false
+		}
+	}
+
+	rs := (p[1] - s[1]) / (p[0] - s[0])
+	ds := (e[1] - s[1]) / (e[0] - s[0])
+
+	if rs == ds {
+		return false, true
+	}
+
+	return rs <= ds, false
+}
---
+++ planar/polygon_test.go
@@ -0,0 +1,252 @@
+package planar
+
+import "testing"
+
+func TestPolygonCentroid(t *testing.T) {
+	cases := []struct {
+		name   string
+		points []Point
+		result Point
+	}{
+		{
+			name:   "triangle, cw",
+			points: []Point{{0, 0}, {1, 3}, {2, 0}, {0, 0}},
+			result: Point{1, 1},
+		},
+		{
+			name:   "triangle, ccw",
+			points: []Point{{0, 0}, {2, 0}, {1, 3}, {0, 0}},
+			result: Point{1, 1},
+		},
+		{
+			name:   "square, cw",
+			points: []Point{{0, 0}, {0, 1}, {1, 1}, {1, 0}, {0, 0}},
+			result: Point{0.5, 0.5},
+		},
+		{
+			name:   "triangle, ccw",
+			points: []Point{{0, 0}, {1, 0}, {1, 1}, {0, 1}, {0, 0}},
+			result: Point{0.5, 0.5},
+		},
+		{
+			name:   "redudent points",
+			points: []Point{{0, 0}, {1, 0}, {2, 0}, {1, 3}, {0, 0}},
+			result: Point{1, 1},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			poly := Polygon{LineString(tc.points)}
+			if c := poly.Centroid(); !c.Equal(tc.result) {
+				t.Errorf("wrong centroid: %v != %v", c, tc.result)
+			}
+		})
+	}
+}
+
+func TestPolygonCentroidAdv(t *testing.T) {
+	ls := append(NewLineString(),
+		NewPoint(0, 0),
+		NewPoint(0, 1),
+		NewPoint(1, 1),
+		NewPoint(1, 0.5),
+		NewPoint(2, 0.5),
+		NewPoint(2, 1),
+		NewPoint(3, 1),
+		NewPoint(3, 0),
+		NewPoint(0, 0),
+	)
+
+	// +-+ +-+
+	// | | | |
+	// | +-+ |
+	// |     |
+	// +-----+
+	poly := Polygon{ls}
+
+	expected := NewPoint(1.5, 0.45)
+	if c := poly.Centroid(); !c.Equal(expected) {
+		t.Errorf("incorrect centroid: %v != %v", c, expected)
+	}
+}
+
+func TestPolygonContains(t *testing.T) {
+	ls := append(NewLineString(),
+		NewPoint(0, 0),
+		NewPoint(0, 1),
+		NewPoint(1, 1),
+		NewPoint(1, 0.5),
+		NewPoint(2, 0.5),
+		NewPoint(2, 1),
+		NewPoint(3, 1),
+		NewPoint(3, 0),
+		NewPoint(0, 0),
+	)
+
+	// +-+ +-+
+	// | | | |
+	// | +-+ |
+	// |     |
+	// +-----+
+	poly := Polygon{ls}
+
+	cases := []struct {
+		name   string
+		point  Point
+		result bool
+	}{
+		{
+			name:   "in base",
+			point:  Point{1.5, 0.25},
+			result: true,
+		},
+		{
+			name:   "in right tower",
+			point:  Point{0.5, 0.75},
+			result: true,
+		},
+		{
+			name:   "in middle",
+			point:  Point{1.5, 0.75},
+			result: false,
+		},
+		{
+			name:   "in left tower",
+			point:  Point{2.5, 0.75},
+			result: true,
+		},
+		{
+			name:   "in tp middle",
+			point:  Point{1.5, 1.0},
+			result: false,
+		},
+		{
+			name:   "above",
+			point:  Point{2.5, 1.75},
+			result: false,
+		},
+		{
+			name:   "below",
+			point:  Point{2.5, -1.75},
+			result: false,
+		},
+		{
+			name:   "left",
+			point:  Point{-2.5, -0.75},
+			result: false,
+		},
+		{
+			name:   "right",
+			point:  Point{3.5, 0.75},
+			result: false,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			val := poly.Contains(tc.point)
+
+			if val != tc.result {
+				t.Errorf("wrong containment: %v != %v", val, tc.result)
+			}
+		})
+	}
+
+	// points should all be in
+	for i, p := range poly[0] {
+		if !poly.Contains(p) {
+			t.Errorf("point index %d: should be inside", i)
+		}
+	}
+
+	// on all the segments should be in.
+	for i := 1; i < len(poly[0]); i++ {
+		c := NewSegment(poly[0][i], poly[0][i-1]).Centroid()
+		if !poly.Contains(c) {
+			t.Errorf("index %d centroid: should be inside", i)
+		}
+	}
+
+	// colinear with segments but outside
+	for i := 1; i < len(poly[0]); i++ {
+		p := NewSegment(poly[0][i], poly[0][i-1]).Interpolate(5)
+		if poly.Contains(p) {
+			t.Errorf("index %d centroid: should not be inside", i)
+		}
+
+		p = NewSegment(poly[0][i], poly[0][i-1]).Interpolate(-5)
+		if poly.Contains(p) {
+			t.Errorf("index %d centroid: should not be inside", i)
+		}
+	}
+}
+
+func TestPolygonArea(t *testing.T) {
+	p1 := append(NewLineString(),
+		NewPoint(0, 0),
+		NewPoint(3, 0),
+		NewPoint(3, 3),
+		NewPoint(0, 3),
+		NewPoint(0, 0),
+	)
+
+	p2 := append(NewLineString(),
+		NewPoint(1, 1),
+		NewPoint(2, 1),
+		NewPoint(2, 2),
+		NewPoint(1, 2),
+		NewPoint(1, 1),
+	)
+
+	polygon := append(NewPolygon(), p1, p2)
+
+	expected := 8.0
+	if a := polygon.Area(); a != expected {
+		t.Errorf("incorrect area: %v != %v", a, expected)
+	}
+}
+
+func TestLineStringArea(t *testing.T) {
+	cases := []struct {
+		name   string
+		points []Point
+		result float64
+	}{
+		{
+			name:   "simple box, ccw",
+			points: []Point{{0, 0}, {1, 0}, {1, 1}, {0, 1}, {0, 0}},
+			result: 1,
+		},
+		{
+			name:   "simple box, cc",
+			points: []Point{{0, 0}, {0, 1}, {1, 1}, {1, 0}, {0, 0}},
+			result: 1,
+		},
+		{
+			name:   "even number of points",
+			points: []Point{{0, 0}, {1, 0}, {1, 1}, {0.4, 1}, {0, 1}, {0, 0}},
+			result: 1,
+		},
+		{
+			name:   "4 points",
+			points: []Point{{0, 0}, {1, 0}, {1, 1}, {0, 0}},
+			result: 0.5,
+		},
+		{
+			name:   "6 points",
+			points: []Point{{1, 1}, {2, 1}, {2, 1.5}, {2, 2}, {1, 2}, {1, 1}},
+			result: 1.0,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			val := lineStringArea(tc.points)
+
+			if val != tc.result {
+				t.Errorf("wrong area: %v != %v", val, tc.result)
+			}
+		})
+	}
+}
