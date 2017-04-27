d784b2cf37b115be794077ff3d8ca78ee7bfb75a
Paul Mach <paulmach@gmail.com>
2017-04-27 16:15:50 -0700

    geojson type constants

---
+++ geo/geojson/types.go
@@ -0,0 +1,9 @@
+package geojson
+
+// A list of the geojson types that are currently supported.
+const (
+	Point      = "Point"
+	MultiPoint = "MultiPoint"
+	LineString = "LineString"
+	Polygon    = "Polygon"
+)
