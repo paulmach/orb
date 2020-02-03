a50c288e09964c9360288d4ee63e3573f0df62f3
Jay Zhang <wangyoucao577@gmail.com>
2020-02-03 12:02:58 +0800

    support download mvt from url directly

--- cmd/vt2geojson/flags.go
+++ cmd/vt2geojson/flags.go
@@ -3,11 +3,11 @@ package main
 import "flag"
 
 var flags struct {
-	mvtFile string
-	layer   string
+	mvtSource string
+	layer     string
 }
 
 func init() {
-	flag.StringVar(&flags.mvtFile, "mvt", "", "Mapbox Vector Tile file path, e.g. 'xxx.mvt' or 'xxx.vector.pbf'.")
+	flag.StringVar(&flags.mvtSource, "mvt", "", "Mapbox Vector Tile file path or URL, e.g. 'xxx.mvt' or 'xxx.vector.pbf' or 'https://api.mapbox.com/v4/mapbox.mapbox-streets-v6/9/150/194.mvt?access_token=YOUR_MAPBOX_ACCESS_TOKEN'.")
 	flag.StringVar(&flags.layer, "layer", "", "Include only the specified layer of the Mapbox Vector Tile.")
 }
--- cmd/vt2geojson/main.go
+++ cmd/vt2geojson/main.go
@@ -5,32 +5,61 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/paulmach/orb/encoding/mvt"
 )
 
+func readMVTContent(mvtSource string) ([]byte, error) {
+
+	if strings.Index(mvtSource, "http") >= 0 { // download from URL
+		resp, err := http.Get(mvtSource)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		content, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		return content, nil
+	}
+
+	// otherwise read from file
+	content, err := ioutil.ReadFile(mvtSource)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
 func main() {
 	flag.Parse()
 
-	if len(flags.mvtFile) > 0 {
-		//TODO: parse mvt file to geojson
-
-		content, err := ioutil.ReadFile(flags.mvtFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		layers, err := mvt.Unmarshal(content)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if len(flags.layer) == 0 {
-			for _, l := range layers {
-				fmt.Printf("layer %s, version %d, extent %d, features %d\n", l.Name, l.Version, l.Extent, len(l.Features))
-			}
-			return
-		}
-		//fmt.Print(layers)
+	if len(flags.mvtSource) == 0 {
+		log.Fatalf("Please specify the mvt source by '-mvt'")
 	}
+
+	//TODO: parse mvt contents to geojson
+
+	content, err := readMVTContent(flags.mvtSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	layers, err := mvt.Unmarshal(content)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(flags.layer) == 0 {
+		for _, l := range layers {
+			fmt.Printf("layer %s, version %d, extent %d, features %d\n", l.Name, l.Version, l.Extent, len(l.Features))
+		}
+		return
+	}
+	//fmt.Print(layers)
 }
