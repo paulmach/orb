dd355325b21b56f8f45a30d4b5cf35b4615450a7
Jay Zhang <wangyoucao577@gmail.com>
2020-02-03 11:19:20 +0800

    print mvt layers summary

--- cmd/vt2geojson/flags.go
+++ cmd/vt2geojson/flags.go
@@ -4,8 +4,10 @@ import "flag"
 
 var flags struct {
 	mvtFile string
+	layer   string
 }
 
 func init() {
-	flag.StringVar(&flags.mvtFile, "f", "", "Mapbox Vector Tile file path, e.g. 'xxx.mvt' or 'xxx.vector.pbf'.")
+	flag.StringVar(&flags.mvtFile, "mvt", "", "Mapbox Vector Tile file path, e.g. 'xxx.mvt' or 'xxx.vector.pbf'.")
+	flag.StringVar(&flags.layer, "layer", "", "Include only the specified layer of the Mapbox Vector Tile.")
 }
--- cmd/vt2geojson/main.go
+++ cmd/vt2geojson/main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 
-	"github.com/golang/glog"
+	"github.com/paulmach/orb/encoding/mvt"
 )
 
 func main() {
 	flag.Parse()
-	defer glog.Flush()
 
 	if len(flags.mvtFile) > 0 {
 		//TODO: parse mvt file to geojson
+
+		content, err := ioutil.ReadFile(flags.mvtFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		layers, err := mvt.Unmarshal(content)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(flags.layer) == 0 {
+			for _, l := range layers {
+				fmt.Printf("layer %s, version %d, extent %d, features %d\n", l.Name, l.Version, l.Extent, len(l.Features))
+			}
+			return
+		}
+		//fmt.Print(layers)
 	}
 }
