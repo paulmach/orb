4aec0134cd2a617d6b497eb5415b93853d2202a9
Aaron Simmons <aaron.simmons@sslmda.com>
2020-10-08 16:47:47 -0700

    #52:adding PropertyXxx and renaming MustXxx to PropertyMustXxx

--- geojson/properties.go
+++ geojson/properties.go
@@ -1,104 +1,132 @@
 package geojson
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Properties defines the feature properties with some helper methods.
 type Properties map[string]interface{}
 
-// MustBool guarantees the return of a `bool` (with optional default).
-// This function useful when you explicitly want a `bool` in a single
-// value return context, for example:
-//     myFunc(f.Properties.MustBool("param1"), f.Properties.MustBool("optional_param", true))
-// This function will panic if the value is present but not a bool.
-func (p Properties) MustBool(key string, def ...bool) bool {
-	v := p[key]
-	if b, ok := v.(bool); ok {
+// SetProperty provides the inverse of all the property functions
+// and is here for consistency.
+func (f *Feature) SetProperty(key string, value interface{}) {
+	if f.Properties == nil {
+		f.Properties = make(map[string]interface{})
+	}
+	f.Properties[key] = value
+}
+
+// PropertyBool type asserts a property to `bool`.
+func (f *Feature) PropertyBool(key string) (bool, error) {
+	if b, ok := (f.Properties[key]).(bool); ok {
+		return b, nil
+	}
+	return false, fmt.Errorf("type assertion of `%s` to bool failed", key)
+}
+
+// PropertyInt type asserts a property to `int`.
+func (f *Feature) PropertyInt(key string) (int, error) {
+	if i, ok := (f.Properties[key]).(int); ok {
+		return i, nil
+	}
+
+	if i, ok := (f.Properties[key]).(float64); ok {
+		return int(i), nil
+	}
+
+	return 0, fmt.Errorf("type assertion of `%s` to int failed", key)
+}
+
+// PropertyFloat64 type asserts a property to `float64`.
+func (f *Feature) PropertyFloat64(key string) (float64, error) {
+	if i, ok := (f.Properties[key]).(float64); ok {
+		return i, nil
+	}
+	return 0, fmt.Errorf("type assertion of `%s` to float64 failed", key)
+}
+
+// PropertyString type asserts a property to `string`.
+func (f *Feature) PropertyString(key string) (string, error) {
+	if s, ok := (f.Properties[key]).(string); ok {
+		return s, nil
+	}
+	return "", fmt.Errorf("type assertion of `%s` to string failed", key)
+}
+
+// PropertyMustBool guarantees the return of a `bool` (with optional default)
+//
+// useful when you explicitly want a `bool` in a single value return context:
+//     myFunc(f.PropertyMustBool("param1"), f.PropertyMustBool("optional_param", true))
+func (f *Feature) PropertyMustBool(key string, def ...bool) bool {
+	var defaul bool
+
+	b, err := f.PropertyBool(key)
+	if err == nil {
 		return b
 	}
 
-	if v != nil {
-		panic(fmt.Sprintf("not a bool, but a %T: %v", v, v))
-	}
-
 	if len(def) > 0 {
-		return def[0]
+		defaul = def[0]
 	}
 
-	panic("property not found")
+	return defaul
 }
 
-// MustInt guarantees the return of an `int` (with optional default).
-// This function useful when you explicitly want a `int` in a single
-// value return context, for example:
-//     myFunc(f.Properties.MustInt("param1"), f.Properties.MustInt("optional_param", 123))
-// This function will panic if the value is present but not a number.
-func (p Properties) MustInt(key string, def ...int) int {
-	v := p[key]
-	if i, ok := v.(int); ok {
-		return i
-	}
+// PropertyMustInt guarantees the return of a `bool` (with optional default)
+//
+// useful when you explicitly want a `bool` in a single value return context:
+//     myFunc(f.PropertyMustInt("param1"), f.PropertyMustInt("optional_param", 123))
+func (f *Feature) PropertyMustInt(key string, def ...int) int {
+	var defaul int
 
-	if f, ok := v.(float64); ok {
-		return int(f)
-	}
-
-	if v != nil {
-		panic(fmt.Sprintf("not a number, but a %T: %v", v, v))
+	b, err := f.PropertyInt(key)
+	if err == nil {
+		return b
 	}
 
 	if len(def) > 0 {
-		return def[0]
+		defaul = def[0]
 	}
 
-	panic("property not found")
+	return defaul
 }
 
-// MustFloat64 guarantees the return of a `float64` (with optional default)
-// This function useful when you explicitly want a `float64` in a single
-// value return context, for example:
-//     myFunc(f.Properties.MustFloat64("param1"), f.Properties.MustFloat64("optional_param", 10.1))
-// This function will panic if the value is present but not a number.
-func (p Properties) MustFloat64(key string, def ...float64) float64 {
-	v := p[key]
-	if f, ok := v.(float64); ok {
-		return f
-	}
+// PropertyMustFloat64 guarantees the return of a `bool` (with optional default)
+//
+// useful when you explicitly want a `bool` in a single value return context:
+//     myFunc(f.PropertyMustFloat64("param1"), f.PropertyMustFloat64("optional_param", 10.1))
+func (f *Feature) PropertyMustFloat64(key string, def ...float64) float64 {
+	var defaul float64
 
-	if i, ok := v.(int); ok {
-		return float64(i)
-	}
-
-	if v != nil {
-		panic(fmt.Sprintf("not a number, but a %T: %v", v, v))
+	b, err := f.PropertyFloat64(key)
+	if err == nil {
+		return b
 	}
 
 	if len(def) > 0 {
-		return def[0]
+		defaul = def[0]
 	}
 
-	panic("property not found")
+	return defaul
 }
 
-// MustString guarantees the return of a `string` (with optional default)
-// This function useful when you explicitly want a `string` in a single
-// value return context, for example:
-//     myFunc(f.Properties.MustString("param1"), f.Properties.MustString("optional_param", "default"))
-// This function will panic if the value is present but not a string.
-func (p Properties) MustString(key string, def ...string) string {
-	v := p[key]
-	if s, ok := v.(string); ok {
-		return s
-	}
+// PropertyMustString guarantees the return of a `bool` (with optional default)
+//
+// useful when you explicitly want a `bool` in a single value return context:
+//     myFunc(f.PropertyMustString("param1"), f.PropertyMustString("optional_param", "default"))
+func (f *Feature) PropertyMustString(key string, def ...string) string {
+	var defaul string
 
-	if v != nil {
-		panic(fmt.Sprintf("not a string, but a %T: %v", v, v))
+	b, err := f.PropertyString(key)
+	if err == nil {
+		return b
 	}
 
 	if len(def) > 0 {
-		return def[0]
+		defaul = def[0]
 	}
 
-	panic("property not found")
+	return defaul
 }
 
 // Clone returns a shallow copy of the properties.
