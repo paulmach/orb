9e0d1bd427b6f3b6f43eac3077f19a9f005cf14d
feiquan123 <feiquanxx@gmail.com>
2021-01-30 23:22:23 +0800

    [#14] add parsing of WKT

---
+++ encoding/wkt/unmarshal.go
@@ -0,0 +1,320 @@
+package wkt
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+
+	"github.com/paulmach/orb"
+)
+
+var (
+	errEmptyGeometry              = errors.New("empty geometry")
+	errUnMarshalPoint             = errors.New("unmarshal point error")
+	errUnMarshalMultiPoint        = errors.New("unmarshal multipoint error")
+	errUnMarshaLineString         = errors.New("unmarshal linestring error")
+	errUnMarshaMultiLineString    = errors.New("unmarshal multilinestring error")
+	errUnMarshaPolygon            = errors.New("unmarshal polygon error")
+	errUnMarshaMultiPolygon       = errors.New("unmarshal multipolygon error")
+	errUnMarshaGEOMETRYCOLLECTION = errors.New("unmarshal collection error")
+)
+
+// errWrap errWarp
+func errWrap(err error, es ...error) error {
+	s := make([]string, 0)
+	if err != nil {
+		s = append(s, err.Error())
+	}
+
+	for _, e := range es {
+		if e != nil {
+			s = append(s, e.Error())
+		}
+	}
+
+	return errors.New(strings.Join(s, "\n"))
+}
+
+// UnmarshalPoint return point by parse wkt point string
+func UnmarshalPoint(s string) (p orb.Point, err error) {
+	geom, err := unmarshal(s)
+	if err != nil {
+		return orb.Point{}, errWrap(err, errEmptyGeometry)
+	}
+	return geom.(orb.Point), nil
+}
+
+// UnmarshalMultiPoint return multipoint by parse wkt multipoint string
+func UnmarshalMultiPoint(s string) (p orb.MultiPoint, err error) {
+	geom, err := unmarshal(s)
+	if err != nil {
+		return orb.MultiPoint{}, errWrap(err, errEmptyGeometry)
+	}
+	return geom.(orb.MultiPoint), nil
+}
+
+// UnmarshalLineString return linestring by parse wkt linestring string
+func UnmarshalLineString(s string) (p orb.LineString, err error) {
+	geom, err := unmarshal(s)
+	if err != nil {
+		return orb.LineString{}, errWrap(err, errEmptyGeometry)
+	}
+	return geom.(orb.LineString), nil
+}
+
+// UnmarshalMultiLineString return linestring by parse wkt multilinestring string
+func UnmarshalMultiLineString(s string) (p orb.MultiLineString, err error) {
+	geom, err := unmarshal(s)
+	if err != nil {
+		return orb.MultiLineString{}, errWrap(err, errEmptyGeometry)
+	}
+	return geom.(orb.MultiLineString), nil
+}
+
+// UnmarshalPolygon return linestring by parse wkt polygon string
+func UnmarshalPolygon(s string) (p orb.Polygon, err error) {
+	geom, err := unmarshal(s)
+	if err != nil {
+		return orb.Polygon{}, errWrap(err, errEmptyGeometry)
+	}
+	return geom.(orb.Polygon), nil
+}
+
+// UnmarshalMultiPolygon return linestring by parse wkt multipolygon string
+func UnmarshalMultiPolygon(s string) (p orb.MultiPolygon, err error) {
+	geom, err := unmarshal(s)
+	if err != nil {
+		return orb.MultiPolygon{}, errWrap(err, errEmptyGeometry)
+	}
+	return geom.(orb.MultiPolygon), nil
+}
+
+// UnmarshalCollection return linestring by parse wkt collection string
+func UnmarshalCollection(s string) (p orb.Collection, err error) {
+	geom, err := unmarshal(s)
+	if err != nil {
+		return orb.Collection{}, errWrap(err, errEmptyGeometry)
+	}
+	return geom.(orb.Collection), nil
+}
+
+// trimSpaceBrackets trim space and brackets
+func trimSpaceBrackets(s string) string {
+	s = strings.Trim(s, " ")
+	if s[0] == '(' {
+		s = s[1:]
+	}
+	if s[len(s)-1] == ')' {
+		s = s[:len(s)-1]
+	}
+	s = strings.Trim(s, " ")
+	return s
+}
+
+// parsePoint pase point by (x y)
+func parsePoint(s string) (p orb.Point, err error) {
+	ps := strings.Split(s, " ")
+	if len(ps) != 2 {
+		return orb.Point{}, errors.New("can't get x,y")
+	}
+	x, err := strconv.ParseFloat(ps[0], 64)
+	if err != nil {
+		return orb.Point{}, err
+	}
+	y, err := strconv.ParseFloat(ps[1], 64)
+	if err != nil {
+		return orb.Point{}, err
+	}
+	p = orb.Point{x, y}
+	return p, nil
+}
+
+// splitGeometryCollection split GEOMETRYCOLLECTION to more geometry
+func splitGeometryCollection(s string) (r []string) {
+	r = make([]string, 0)
+	stack := make([]rune, 0)
+	l := len(s)
+	for i, v := range s {
+		if !strings.Contains(string(stack), "(") {
+			stack = append(stack, v)
+			continue
+		}
+		if v >= 'A' && v < 'Z' {
+			t := string(stack)
+			r = append(r, t[:len(t)-1])
+			stack = make([]rune, 0)
+			stack = append(stack, v)
+			continue
+		}
+		if i == l-1 {
+			r = append(r, string(stack))
+			continue
+		}
+		stack = append(stack, v)
+	}
+	return
+}
+
+/*
+unmarshal return a geometry by parse wkt string
+order:
+	GEOMETRYCOLLECTION
+	MULTIPOINT
+	POINT
+	MULTILINESTRING
+	LINESTRING
+	MULTIPOLYGON
+	POLYGON
+*/
+func unmarshal(s string) (geom orb.Geometry, err error) {
+	s = strings.ToUpper(strings.Trim(s, " "))
+	switch {
+	case strings.Contains(s, "GEOMETRYCOLLECTION"):
+		if s == "GEOMETRYCOLLECTION " {
+			return orb.Collection{}, nil
+		}
+		s = strings.Replace(s, "GEOMETRYCOLLECTION", "", -1)
+		c := orb.Collection{}
+		ms := splitGeometryCollection(s)
+		if len(ms) == 0 {
+			return nil, errUnMarshaGEOMETRYCOLLECTION
+		}
+		for _, v := range ms {
+			if len(v) == 0 {
+				continue
+			}
+			g, err := unmarshal(v)
+			if err != nil {
+				return nil, errWrap(errUnMarshaGEOMETRYCOLLECTION, err)
+			}
+			c = append(c, g)
+		}
+		geom = c
+
+	case strings.Contains(s, "MULTIPOINT"):
+		if s == "MULTIPOINT EMPTY" {
+			return orb.MultiPoint{}, nil
+		}
+		s = strings.Replace(s, "MULTIPOINT", "", -1)
+		s = trimSpaceBrackets(s)
+		ps := strings.Split(s, ",")
+		mp := orb.MultiPoint{}
+		for _, p := range ps {
+			tp, err := parsePoint(trimSpaceBrackets(p))
+			if err != nil {
+				return nil, errWrap(errUnMarshalPoint, err)
+			}
+			mp = append(mp, tp)
+		}
+		geom = mp
+
+	case strings.Contains(s, "POINT"):
+		s = strings.Replace(s, "POINT", "", -1)
+		tp, err := parsePoint(trimSpaceBrackets(s))
+		if err != nil {
+			return nil, errWrap(errUnMarshalPoint, err)
+		}
+		geom = tp
+
+	case strings.Contains(s, "MULTILINESTRING"):
+		if s == "MULTILINESTRING EMPTY" {
+			return orb.MultiLineString{}, nil
+		}
+		s = strings.Replace(s, "MULTILINESTRING", "", -1)
+		ml := orb.MultiLineString{}
+		for _, l := range strings.Split(trimSpaceBrackets(s), "),(") {
+			tl := orb.LineString{}
+			for _, p := range strings.Split(trimSpaceBrackets(l), ",") {
+				tp, err := parsePoint(trimSpaceBrackets(p))
+				if err != nil {
+					return nil, errWrap(errUnMarshaMultiLineString, err)
+				}
+				tl = append(tl, tp)
+			}
+			ml = append(ml, tl)
+		}
+		geom = ml
+
+	case strings.Contains(s, "LINESTRING"):
+		if s == "LINESTRING EMPTY" {
+			return orb.LineString{}, nil
+		}
+		s = strings.Replace(s, "LINESTRING", "", -1)
+		s = trimSpaceBrackets(s)
+		ps := strings.Split(s, ",")
+		ls := orb.LineString{}
+		for _, p := range ps {
+			tp, err := parsePoint(trimSpaceBrackets(p))
+			if err != nil {
+				return nil, errWrap(errUnMarshaLineString, err)
+			}
+			ls = append(ls, tp)
+		}
+		geom = ls
+
+	case strings.Contains(s, "MULTIPOLYGON"):
+		if s == "MULTIPOLYGON EMPTY" {
+			return orb.MultiPolygon{}, nil
+		}
+		s = strings.Replace(s, "MULTIPOLYGON", "", -1)
+		mpol := orb.MultiPolygon{}
+		for _, ps := range strings.Split(trimSpaceBrackets(s), ")),((") {
+			pol := orb.Polygon{}
+			for _, ls := range strings.Split(trimSpaceBrackets(ps), "),(") {
+				ring := orb.Ring{}
+				for _, p := range strings.Split(ls, ",") {
+					tp, err := parsePoint(trimSpaceBrackets(p))
+					if err != nil {
+						return nil, errWrap(errUnMarshaMultiPolygon, err)
+					}
+					ring = append(ring, tp)
+				}
+				pol = append(pol, ring)
+			}
+			mpol = append(mpol, pol)
+		}
+		geom = mpol
+
+	case strings.Contains(s, "POLYGON"):
+		if s == "POLYGON EMPTY" {
+			return orb.Polygon{}, nil
+		}
+		s = strings.Replace(s, "POLYGON", "", -1)
+		s = trimSpaceBrackets(s)
+		rs := strings.Split(s, "),(")
+		if len(rs) == 1 {
+			// ring
+			ps := strings.Split(trimSpaceBrackets(s), ",")
+			ring := orb.Ring{}
+			for _, p := range ps {
+				tp, err := parsePoint(trimSpaceBrackets(p))
+				if err != nil {
+					return nil, errWrap(errUnMarshaLineString, err)
+				}
+				ring = append(ring, tp)
+			}
+			geom = orb.Polygon{ring}
+		} else {
+			// more ring
+			pol := orb.Polygon{}
+			for _, r := range rs {
+				ps := strings.Split(trimSpaceBrackets(r), ",")
+				ring := orb.Ring{}
+				for _, p := range ps {
+					tp, err := parsePoint(trimSpaceBrackets(p))
+					if err != nil {
+						return nil, errWrap(errUnMarshaLineString, err)
+					}
+					ring = append(ring, tp)
+				}
+				pol = append(pol, ring)
+			}
+			geom = pol
+		}
+
+	default:
+		panic("wkt: unsupported geometry")
+	}
+
+	return
+}
---
+++ encoding/wkt/unmarshal_test.go
@@ -0,0 +1,276 @@
+package wkt
+
+import (
+	"testing"
+
+	"github.com/paulmach/orb"
+)
+
+func TestTrimSpaceBrackets(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected string
+	}{
+		{
+			s:        "(1 2)",
+			expected: "1 2",
+		},
+		{
+			s:        "((1 2),(0.5 1.5))",
+			expected: "(1 2),(0.5 1.5)",
+		},
+		{
+			s:        "(1 2,0.5 1.5)",
+			expected: "1 2,0.5 1.5",
+		},
+		{
+			s:        "((1 2,3 4),(5 6,7 8))",
+			expected: "(1 2,3 4),(5 6,7 8)",
+		},
+		{
+			s:        "(((1 2,3 4)),((5 6,7 8),(1 2,5 4)))",
+			expected: "((1 2,3 4)),((5 6,7 8),(1 2,5 4))",
+		},
+	}
+
+	for _, tc := range cases {
+		if trimSpaceBrackets(tc.s) != tc.expected {
+			t.Log(trimSpaceBrackets(tc.s))
+			t.Log(tc.expected)
+			t.Errorf("trim space and brackets error")
+		}
+	}
+}
+
+func TestUnmarshalPoint(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected orb.Point
+	}{
+		// int
+		{
+			s:        "POINT(1 2)",
+			expected: orb.Point{1, 2},
+		},
+		// float64
+		{
+			s:        "POINT(1.34 2.35)",
+			expected: orb.Point{1.34, 2.35},
+		},
+	}
+
+	for _, tc := range cases {
+		geom, err := UnmarshalPoint(tc.s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !geom.Equal(tc.expected) {
+			t.Log(geom)
+			t.Log(tc.expected)
+			t.Errorf("incorrect wkt unmarshalling")
+		}
+	}
+}
+
+func TestUnmarshalMultiPoint(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected orb.MultiPoint
+	}{
+		// empty
+		{
+			s:        "MULTIPOINT EMPTY",
+			expected: orb.MultiPoint{},
+		},
+		// int
+		{
+			s:        "MULTIPOINT((1 2),(0.5 1.5))",
+			expected: orb.MultiPoint{{1, 2}, {0.5, 1.5}},
+		},
+	}
+
+	for _, tc := range cases {
+		geom, err := UnmarshalMultiPoint(tc.s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !geom.Equal(tc.expected) {
+			t.Log(geom)
+			t.Log(tc.expected)
+			t.Errorf("incorrect wkt unmarshalling")
+		}
+	}
+}
+
+func TestUnmarshalLineString(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected orb.LineString
+	}{
+		{
+			s:        "LINESTRING EMPTY",
+			expected: orb.LineString{},
+		},
+
+		{
+			s:        "LINESTRING(1 2,0.5 1.5)",
+			expected: orb.LineString{{1, 2}, {0.5, 1.5}},
+		},
+	}
+
+	for _, tc := range cases {
+		geom, err := UnmarshalLineString(tc.s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !geom.Equal(tc.expected) {
+			t.Log(geom)
+			t.Log(tc.expected)
+			t.Errorf("incorrect wkt unmarshalling")
+		}
+	}
+}
+
+func TestUnmarshalMultiLineString(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected orb.MultiLineString
+	}{
+		{
+			s:        "MULTILINESTRING EMPTY",
+			expected: orb.MultiLineString{},
+		},
+
+		{
+			s:        "MULTILINESTRING((1 2,3 4),(5 6,7 8))",
+			expected: orb.MultiLineString{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}},
+		},
+	}
+
+	for _, tc := range cases {
+		geom, err := UnmarshalMultiLineString(tc.s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !geom.Equal(tc.expected) {
+			t.Log(geom)
+			t.Log(tc.expected)
+			t.Errorf("incorrect wkt unmarshalling")
+		}
+	}
+}
+
+func TestUnmarshalPolygon(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected orb.Polygon
+	}{
+		// empty
+		{
+			s:        "POLYGON EMPTY",
+			expected: orb.Polygon{},
+		},
+		// ring
+		// origin: orb.Ring{{0, 0}, {1, 0}, {1, 1}, {0, 0}}
+		// convert: orb.Polygon{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}}
+		{
+			s:        "POLYGON((0 0,1 0,1 1,0 0))",
+			expected: orb.Polygon{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}},
+		},
+		// bound
+		// origin: orb.Bound{Min: orb.Point{0, 0}, Max: orb.Point{1, 2}},
+		// convert: orb.Polygon{{{0, 0}, {1, 0}, {1, 2}, {0, 2}, {0, 0}}}
+		{
+			s:        "POLYGON((0 0,1 0,1 2,0 2,0 0))",
+			expected: orb.Polygon{{{0, 0}, {1, 0}, {1, 2}, {0, 2}, {0, 0}}},
+		},
+		// polygon
+		{
+			s:        "POLYGON((1 2,3 4),(5 6,7 8))",
+			expected: orb.Polygon{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}},
+		},
+	}
+
+	for _, tc := range cases {
+		geom, err := UnmarshalPolygon(tc.s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !geom.Equal(tc.expected) {
+			t.Log(geom)
+			t.Log(tc.expected)
+			t.Errorf("incorrect wkt unmarshalling")
+		}
+	}
+}
+
+func TestUnmarshalMutilPolygon(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected orb.MultiPolygon
+	}{
+		// empty
+		{
+			s:        "MULTIPOLYGON EMPTY",
+			expected: orb.MultiPolygon{},
+		},
+		// multi polygon
+		{
+			s:        "MULTIPOLYGON(((1 2,3 4)),((5 6,7 8),(1 2,5 4)))",
+			expected: orb.MultiPolygon{{{{1, 2}, {3, 4}}}, {{{5, 6}, {7, 8}}, {{1, 2}, {5, 4}}}},
+		},
+	}
+
+	for _, tc := range cases {
+		geom, err := UnmarshalMultiPolygon(tc.s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !geom.Equal(tc.expected) {
+			t.Log(geom)
+			t.Log(tc.expected)
+			t.Errorf("incorrect wkt unmarshalling")
+		}
+	}
+}
+
+func TestUnmarshalCollection(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected orb.Collection
+	}{
+		// empty
+		{
+			s:        "GEOMETRYCOLLECTION EMPTY",
+			expected: orb.Collection{},
+		},
+		// multi polygon
+		{
+			s:        "GEOMETRYCOLLECTION(POINT(1 2),LINESTRING(3 4,5 6))",
+			expected: orb.Collection{orb.Point{1, 2}, orb.LineString{{3, 4}, {5, 6}}},
+		},
+		{
+			s: "GEOMETRYCOLLECTION(POINT(1 2),LINESTRING(3 4,5 6),MULTILINESTRING((1 2,3 4),(5 6,7 8)),POLYGON((0 0,1 0,1 1,0 0)),POLYGON((1 2,3 4),(5 6,7 8)),MULTIPOLYGON(((1 2,3 4)),((5 6,7 8),(1 2,5 4)))",
+			expected: orb.Collection{
+				orb.Point{1, 2},
+				orb.LineString{{3, 4}, {5, 6}},
+				orb.MultiLineString{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}},
+				orb.Polygon{{{0, 0}, {1, 0}, {1, 1}, {0, 0}}},
+				orb.Polygon{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}},
+				orb.MultiPolygon{{{{1, 2}, {3, 4}}}, {{{5, 6}, {7, 8}}, {{1, 2}, {5, 4}}}},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		geom, err := UnmarshalCollection(tc.s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !geom.Equal(tc.expected) {
+			t.Log(geom)
+			t.Log(tc.expected)
+			t.Errorf("incorrect wkt unmarshalling")
+		}
+	}
+}
