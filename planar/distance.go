3ef56d5fc669258314ba0445871cb24e1afd340f
sumsam <sumsam.ullah@voiapp.io>
2021-08-26 18:48:13 +0200

    feat: add implementation for haversine distance for geo-coordinates

--- planar/distance.go
+++ planar/distance.go
@@ -6,6 +6,11 @@ import (
 	"github.com/paulmach/orb"
 )
 
+const (
+	// EarthRadius is the Earth's radius in kilometers
+	EarthRadius = 6371
+)
+
 // Distance returns the distance between two points in 2d euclidean geometry.
 func Distance(p1, p2 orb.Point) float64 {
 	d0 := (p1[0] - p2[0])
@@ -18,4 +23,23 @@ func DistanceSquared(p1, p2 orb.Point) float64 {
 	d0 := (p1[0] - p2[0])
 	d1 := (p1[1] - p2[1])
 	return d0*d0 + d1*d1
+}
+
+// HaversineDistance calculates the distance between two points on earth in kilometers
+// Implementation from: http://www.movable-type.co.uk/scripts/latlong.html
+func HaversineDistance(p1, p2 orb.Point) float64 {
+	dLat := (p2[1] - p1[1]) * (math.Pi / 180.0)
+	dLon := (p2[0] - p1[0]) * (math.Pi / 180.0)
+
+	lat1 := p1[1] * (math.Pi / 180.0)
+	lat2 := p2[1] * (math.Pi / 180.0)
+
+	a1 := math.Sin(dLat/2) * math.Sin(dLat/2)
+	a2 := math.Sin(dLon/2) * math.Sin(dLon/2) * math.Cos(lat1) * math.Cos(lat2)
+
+	a := a1 + a2
+
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	return EarthRadius * c
 }
