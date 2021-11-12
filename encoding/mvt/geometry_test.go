e58bd12bd054520058f8da0ed7753497ed6e03d5
Paul Mach <paul@mach.org>
2021-11-11 18:46:48 -0800

    add test for PR #78

--- encoding/mvt/geometry_test.go
+++ encoding/mvt/geometry_test.go
@@ -173,15 +173,30 @@ func TestGeometry_MultiPolygon(t *testing.T) {
 
 func TestKeyValueEncoder_JSON(t *testing.T) {
 	kve := newKeyValueEncoder()
-	i, err := kve.Value([]int{1, 2, 3})
-	if err != nil {
-		t.Fatalf("failed to get value: %v", err)
-	}
 
-	value := decodeValue(kve.Values[i])
-	if value != "[1,2,3]" {
-		t.Errorf("should encode non standard types as json")
-	}
+	t.Run("non comparable value", func(t *testing.T) {
+		i, err := kve.Value([]int{1, 2, 3})
+		if err != nil {
+			t.Fatalf("failed to get value: %v", err)
+		}
+
+		value := decodeValue(kve.Values[i])
+		if value != "[1,2,3]" {
+			t.Errorf("should encode non standard types as json")
+		}
+	})
+
+	t.Run("nil value", func(t *testing.T) {
+		i, err := kve.Value(nil)
+		if err != nil {
+			t.Fatalf("failed to get value: %v", err)
+		}
+
+		value := decodeValue(kve.Values[i])
+		if value != "null" {
+			t.Errorf("should encode null as json")
+		}
+	})
 }
 
 type stringer int
